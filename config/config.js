69b96b34321fbd3af29bff598f60eb7c7c381ca6
Joscandy Nunez <50030017+jcandyn@users.noreply.github.com>
2022-09-24 02:27:43 -0400

    Update config

--- config/config.js
+++ config/config.js
@@ -9,14 +9,6 @@ module.exports =
     "port": 3306,
     "dialect": "mysql"
   },
-  "test": {
-    "username": "f1u2joh9cplot7jh",
-    "password": "yrcokmrcpq2trml9",
-    "database": "iudzmc43bfr7ut00",
-    "host": "xq7t6tasopo9xxbs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    "port": 3306,
-    "dialect": "mysql"
-  },
   "production": {
     "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
